43584fa0a2aafadf9b59e9e30197e5531d432647
Наталья <109668015+andre19071@users.noreply.github.com>
2022-09-02 21:26:50 +0700

    task69

--- task69/Program.cs
+++ task69/Program.cs
@@ -2,3 +2,19 @@
 // A и B, и возводит число А в целую степень B с помощью рекурсии.
 // A = 3; B = 5 -> 243 (3⁵)
 // A = 2; B = 3 -> 8
+
+Console.WriteLine("Введите число A");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int numberB = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(PowAtoB(numberA, numberB));
+
+int PowAtoB(int a, int b)
+
+{
+    if ( b == 0) return 1;
+    return PowAtoB(a, b - 1) * a;
+}
+
+
